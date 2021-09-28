9de424585e2053a045939fe8398139c11c438fb2
Iqrakhan08 <khaniqra0808@gmail.com>
2021-09-28 21:09:19 +0530

    add nutition

--- client/src/Components/Dining/DiningCarousel.jsx
+++ client/src/Components/Dining/DiningCarousel.jsx
@@ -7,6 +7,7 @@ import { NextArrow,PrevArrow } from "../CarouselArrow";
 
 const DiningCarousel=()=>{
     const settings={
+        dots:true,
         arrows:true,
         infinite:true,
         speed:500,
@@ -14,6 +15,32 @@ const DiningCarousel=()=>{
         slidesToScroll:1,
         nextArrow:<NextArrow/>,
         prevArrow:<PrevArrow/>,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true,
+              },
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                initialSlide: 2,
+              },
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+              },
+            },
+          ],
     };
     return(
         <div className="w-full">
--- client/src/Components/NightLife/NightLifeCarousel.jsx
+++ client/src/Components/NightLife/NightLifeCarousel.jsx
@@ -7,13 +7,41 @@ import { NextArrow,PrevArrow } from "../CarouselArrow";
 
 const NightLifeCarousel=()=>{
     const settings={
+        dots:true,
         arrows:true,
         infinite:true,
         speed:500,
         slidesToShow:4,
         slidesToScroll:1,
+        initialSlide:0,
         nextArrow:<NextArrow/>,
         prevArrow:<PrevArrow/>,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true,
+              },
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                initialSlide: 2,
+              },
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+              },
+            },
+          ],
     };
     return(
         <div className="w-full">
