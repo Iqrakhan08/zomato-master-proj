1c90275f1aefcd11ccb47b285256acec8328d40b
Iqrakhan08 <khaniqra0808@gmail.com>
2021-09-28 10:24:36 +0530

    add dining carousel

--- client/src/Components/master.jsx
+++ client/src/Components/master.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import { useParams } from "react-router-dom";
 //components
 import Delivery from "./Delivery";
+import Dining from "./Dining";
 
 const Master=()=>{
 const{type}=useParams();
- return <div className="my-5">
+ return (
+   <div className="my-5">
   {type==="delivery"&&<Delivery/>}
- </div>;
+  {type==="dining"&&<Dining/>}
+ </div>
+ );
 };
 
 export default Master;
