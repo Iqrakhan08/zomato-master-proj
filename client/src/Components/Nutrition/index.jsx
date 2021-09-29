7aabf77cc896359962c624ecbfbe522bc543b791
Iqrakhan08 <khaniqra0808@gmail.com>
2021-09-29 17:22:54 +0530

    add nutition card

--- client/src/Components/Nutrition/index.jsx
+++ client/src/Components/Nutrition/index.jsx
@@ -3,6 +3,7 @@ import React from "react";
 //components
 import NutritionHeroCarousel from "./NutritionHeroCarousel";
 import NutritionCarousel from "./NutritionCarousel";
+import NutritionCard from "./NutritionCard";
 
 const Nutrition=()=>{
     return (
@@ -11,6 +12,24 @@ const Nutrition=()=>{
             <div className="my-6">
             <NutritionCarousel/>
             </div>
+            <div className="flex flex-wrap justify-between">
+                <NutritionCard 
+                bg="red"
+                image="https://dote.zmtcdn.com/prod/data/admin_assets/images/985/1db983abf8a38bdebb42171b5ea7d985_1614756360.png?output-format=webp"
+              />
+                </div>
+                <div className="flex flex-wrap justify-between">
+                <NutritionCard 
+                bg="red"
+                image="https://dote.zmtcdn.com/prod/data/admin_assets/images/985/1db983abf8a38bdebb42171b5ea7d985_1614756360.png?output-format=webp"
+              />
+                </div>
+                <div className="flex flex-wrap justify-between">
+                <NutritionCard 
+                bg="red"
+                image="https://dote.zmtcdn.com/prod/data/admin_assets/images/985/1db983abf8a38bdebb42171b5ea7d985_1614756360.png?output-format=webp"
+              />
+                </div>
         </div>
     );
 };
